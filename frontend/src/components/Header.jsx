977742e6627f1f8755d715c6dba39630a7428071
Pratham Darji <155282402+Pratham-Prog861@users.noreply.github.com>
2025-10-03 12:41:41 +0530

    update title and header text to 'VidSpace'

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = ({ onToggleSidebar, sidebarCollapsed }) => {
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
               </svg>
             </div>
-            <span className="text-white font-semibold text-xl hidden sm:block">StreamVibe</span>
+            <span className="text-white font-semibold text-xl hidden sm:block">VidSpace</span>
           </div>
         </div>
 
